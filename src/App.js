fc9220351b9f612404d89b485c215baa1bc8fad6
letsummer <letsummerparty@gmail.com>
2024-01-12 14:42:33 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { HashRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Home from './component/Home';
+import Profile from './component/Profile';
+import Projects from './component/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to={"/"}>홈</Link></li>
+            <li><Link to={"/profile"}>프로필</Link></li>
+            <li><Link to={"/projects"}>프로젝트</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+        </Routes>
+    </Router>
     </div>
   );
 }
