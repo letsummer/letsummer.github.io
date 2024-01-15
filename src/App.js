eb989986d55e6e20c196bfa8a7f211ac592db7b4
letsummer <letsummerparty@gmail.com>
2024-01-15 16:10:35 +0900

    메인 js 파일 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { HashRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import './App.css';
+// import './App.css';
+import styles from './css/layout.module.css';
 import Home from './component/Home';
 import Profile from './component/Profile';
 import Projects from './component/Projects';
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
       <Router>
-        <nav>
+        <nav className={styles.navigation}>
           <ul>
             <li><Link to={"/"}>홈</Link></li>
             <li><Link to={"/profile"}>프로필</Link></li>
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/profile" element={<Profile/>}></Route>
           <Route path="/projects" element={<Projects/>}></Route>
+          {/* <Route path="/projects/:id" element={<Detail/>}></Route> */}
         </Routes>
     </Router>
     </div>
